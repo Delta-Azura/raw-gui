52b0da88b6bffc0e7db4c2b646c61af93bb4e437
Delta-Azura <alexis.prev.delta07@gmail.com>
2026-05-24 00:35:07 +0200

    license

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+// CARDS-GUI is a simple GUI interface for cards written in Rust using ICED.
+//    Copyright (C) 2026  Alexis/Delta-Azura
+
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License along
+//    with this program; if not, write to the Free Software Foundation, Inc.,
+//    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
+
 use iced::widget::{button, column, container, row, scrollable, text, Button};
 use iced::{Background, Border, Color, Element, Length, Theme};
 use std::fs;
